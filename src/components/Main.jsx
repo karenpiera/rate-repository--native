c001447f95444bf265b4f942a202222dff915f2d
karenpiera <karengp@hotmail.com.ar>
2023-12-13 17:39:57 -0300

    routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,45 @@
+// import { Text, View } from "react-native";
+// import Constants from "expo-constants";
+// import React from "react";
+// import RepositoryList from "./RepositoryList";
+// import AppBar from "./AppBar";
+// import { Redirect, Switch, Route, Routes } from "react-router-native";
+
+// function Main() {
+//   return (
+//     <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
+//       <AppBar />
+//       <Routes>
+//         <Route path="/" exact>
+//           <RepositoryList />
+//         </Route>
+//         <Route>
+//           <Text> Working on it </Text>
+//         </Route>
+//         <Redirect to="/" />
+//       </Routes>
+//     </View>
+//   );
+// }
+
+// export default Main;
+
 import { Text, View } from "react-native";
 import Constants from "expo-constants";
 import React from "react";
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Route, Routes, Redirect } from "react-router-native";
 
 function Main() {
   return (
     <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-      <Text>Main</Text>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/working" element={<Text>Working on it</Text>} />
+        <Route path="*" element={<Redirect to="/" />} />
+      </Routes>
     </View>
   );
 }
